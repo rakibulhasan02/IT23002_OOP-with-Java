884f69b8aa59b79eea3df4b1a53195159dc0c780
Md Rakibul Hasan <152282042+rakibulhasan02@users.noreply.github.com>
2024-11-18 00:58:19 +0600

    Update Ch1Ex1.java

--- Chapter_1/Ch1Ex1.java
+++ Chapter_1/Ch1Ex1.java
@@ -1,1 +1,7 @@
-
+class Ch1Ex1{
+    public static void main(String[] args){
+        System.out.println("Welcome to Java\nWelcome to Computer Science\nProgramming is fun.");
+    }
+}
+//Md Rakibul Hasan
+//IT23002
