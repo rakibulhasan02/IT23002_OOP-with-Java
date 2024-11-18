2a7a056632958b2dc54ff5b32a90bb925d9a5ad9
Md Rakibul Hasan <152282042+rakibulhasan02@users.noreply.github.com>
2024-11-18 20:31:09 +0600

    Add files via upload
    IT23002

---
+++ Chapter_1/Ch1Ex9.java
@@ -0,0 +1,8 @@
+public class Ch1Ex9 {
+    public static void main(String[] args){
+        double height=7.9,width=4.5;
+        double area=height*width;
+        System.out.println("Area of rectingle = "+area);
+    }
+}
+//IT23002
