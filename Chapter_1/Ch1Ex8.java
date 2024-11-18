45618a6c9ed62b5211e4af58f9f073a63f2b9cd0
Md Rakibul Hasan <152282042+rakibulhasan02@users.noreply.github.com>
2024-11-18 20:21:00 +0600

    Add files via upload
    IT23002

---
+++ Chapter_1/Ch1Ex8.java
@@ -0,0 +1,10 @@
+public class Ch1Ex8 {
+    public static void main(String[] args){
+        double radius=5.5;
+        double area,perimeter;
+        double pi=Math.PI;
+        System.out.println("Area of circle = "+pi*radius*radius);
+        System.out.println("Perimeter of circle = "+2.0*pi*radius);
+    }
+}
+//IT23002
