12d93713a22a6e3e286a7581e865c28fbc923c4d
Md Rakibul Hasan <152282042+rakibulhasan02@users.noreply.github.com>
2024-11-18 20:00:06 +0600

    Add files via upload
    IT23002

---
+++ Chapter_1/Ch1ex6.java
@@ -0,0 +1,11 @@
+
+public class Ch1ex6 {
+    public static void main(String[] args){
+        int sum=0;
+        for(int i=1;i<=9;i++){
+            sum+=i;
+        }
+        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = "+sum);
+
+    }
+}
